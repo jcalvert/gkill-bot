3cbeadfea159543f4013a6713e8f9dcbe4c184e5
Jonathan Calvert <athemeus@athemeus.com>
2018-03-03 00:05:01 -0500

    more explore

--- lib/gkill-bot.rb
+++ lib/gkill-bot.rb
@@ -1,6 +1,8 @@
+require 'pry'
 require 'yaml'
 require 'discordrb'
 
+#TODO Organize this crap
 module Gkill
   class Bot
 
@@ -8,11 +10,17 @@ module Gkill
       conf = YAML::load_file('config.yml')
       bot = Discordrb::Bot.new token: conf['token'], client_id: conf['client_id']
 
-      command_bot = Discordrb::Commands::CommandBot.new token: conf['token'], client_id: conf['client_id'],  prefix: '!'
+      @rollcall = [] #obv replace with orm
 
+      command_bot = Discordrb::Commands::CommandBot.new token: conf['token'], client_id: conf['client_id'], prefix: "!"
 
-      command_bot.command :bold do |_event, *args|
-        "**#{args.join(' ')}**"
+      command_bot.command(:checkin, description: '', usage: '') do |event|
+        binding.pry
+        "You are in my soul!"
+      end
+
+      command_bot.command(:rollcall, description: '', usage: '') do |event|
+        "Current checked in players: #{@rollcall.join(', ')}"
       end
 
       bot.message(content: 'Ping!') do |event|
@@ -27,8 +35,13 @@ module Gkill
         event.respond 'My lord and master.'
       end
 
+      bot.message(content: 'Who is OddBall 39?') do |event|
+        event.respond 'Tor up from the floor up! https://i.pinimg.com/736x/90/9b/55/909b5599394a390796e07c0fab54092f--kellys-heroes-donald-sutherland.jpg'
+      end
+
+      Thread.new { command_bot.run }
+
       bot.run
-      command_bot.run
     end
 
   end
