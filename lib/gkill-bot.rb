d76d31bb2e3aeaec6209ce4f14015a03792e7fe3
Jonathan Calvert <athemeus@athemeus.com>
2018-03-02 20:49:42 -0500

    ok

--- lib/gkill-bot.rb
+++ lib/gkill-bot.rb
@@ -1,37 +1,26 @@
 require 'yaml'
 require 'discordrb'
 
-# This statement creates a bot with the specified token and application ID. After this line, you can add events to the
-# created bot, and eventually run it.
-#
-# If you don't yet have a token to put in here, you will need to create a bot account here:
-#   https://discordapp.com/developers/applications/me
-# If you're wondering about what redirect URIs and RPC origins, you can ignore those for now. If that doesn't satisfy
-# you, look here: https://github.com/meew0/discordrb/wiki/Redirect-URIs-and-RPC-origins
-# After creating the bot, simply copy the token (*not* the OAuth2 secret) and put it into the
-# respective place.
-conf = YAML::load_file('config.yml')
-bot = Discordrb::Bot.new token: conf['token'], client_id: conf['client_id']
+module Gkill
+  class Bot
 
-# Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
-# done once, afterwards, you can remove this part if you want
-puts "This bot's invite URL is #{bot.invite_url}."
-puts 'Click on it to invite it to your server.'
+    def self.run
+      conf = YAML::load_file('config.yml')
+      bot = Discordrb::Bot.new token: conf['token'], client_id: conf['client_id']
+      bot.message(content: 'Ping!') do |event|
+        event.respond 'Pong!'
+      end
 
-# This method call adds an event handler that will be called on any message that exactly contains the string "Ping!".
-# The code inside it will be executed, and a "Pong!" response will be sent to the channel.
-bot.message(content: 'Ping!') do |event|
-  event.respond 'Pong!'
+      bot.message(content: 'What is best in life?') do |event|
+        event.respond 'To crush your enemies. See them driven before you. And to hear the lamentations of their women.'
+      end
+
+      bot.message(content: 'Who is Athemeus?') do |event|
+        event.respond 'My lord and master.'
+      end
+
+      bot.run
+    end
+
+  end
 end
-
-bot.message(content: 'What is best in life?') do |event|
-  event.respond 'To crush your enemies. See them driven before you. And to hear the lamentations of their women.'
-end
-
-bot.message(content: 'Who is Athemeus?') do |event|
-  event.respond 'My lord and master.'
-end
-
-# This method call has to be put at the end of your script, it is what makes the bot actually connect to Discord. If you
-# leave it out (try it!) the script will simply stop and the bot will not appear online.
-bot.run
